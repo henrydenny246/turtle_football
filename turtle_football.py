00bc3d06285aa1ab77a1753c152d2c47cf5f1cfc
henrydenny246 <henry.denny12@gmail.com>
2016-12-22 11:09:36 +0000

    Add files via upload

---
+++ turtle_football.py
@@ -0,0 +1,16 @@
+#Turtle Football
+import turtle
+import time
+
+wn = turtle.Screen()
+p1 = turtle.Turtle()
+p2 = turtle.Turtle()
+pMaker = turtle.Turtle()
+scoreBoard = turtle.Turtle()
+p1ScorePrinter = turtle.Turtle()
+p2ScorePrinter = turtle.Turtle()
+
+wn.bgcolor("lightgreen")
+wn.setworldcoordinates(-1000,-7.5,50,7.5)
+pMaker.speed(0)
+pMaker.color("white")
